c0fbbf42610db16df8cd533ff823bcf65ba99fc8
BuildTools <mojweb123@gmail.com>
2017-09-26 20:52:34 +0200

    updated cusercmd

--- csgo-base/src/sdk/CUserCmd.h
+++ csgo-base/src/sdk/CUserCmd.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "../vector.h"
 #include "../sdk.h"
-
+/* 
 class CUserCmd
 {
 private:
@@ -22,4 +22,25 @@ public:
 	bool	hasbeenpredicted;
 private:
 	char	pad01[24];
+}; */
+struct CUserCmd
+{
+	virtual ~CUserCmd() {};
+	int command_number;
+	int tick_count;
+	vec3f viewangles;
+	vec3f aimdirection;
+	float forwardmove;
+	float sidemove;
+	float upmove;
+	int buttons;
+	unsigned char impulse;
+	int weaponselect;
+	int weaponsubtype;
+	int random_seed;
+	short mousedx;
+	short mousedy;
+	bool hasbeenpredicted;
+	vec3f headangles;
+	vec3f headoffset;
 };
