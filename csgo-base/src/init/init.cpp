64da515a0b8ad473f080c3dcea4a2ec3b35ca6d6
BuildTools <mojweb123@gmail.com>
2017-09-30 15:43:39 +0200

    now using the new string class

--- csgo-base/src/init/init.cpp
+++ csgo-base/src/init/init.cpp
@@ -40,42 +40,43 @@ void SetupConsole()
     freopen("CONIN$", "rb", stdin);
     SetConsoleTitle("Debug");
 }
-
+#include "../util/string_constex.h"
 bool init::setup()
 {
     SetupConsole();
+
 #ifndef __TOUCH_TIER_0__
     g_Msg = util::getexport<MsgFn>("tier0.dll", "Msg");
     g_Warning = util::getexport<WarningFn>("tier0.dll", "Warning");
     g_Msg("aaa\n");
 #endif
 
-    g_pClient = (CClientBase *)util::EasyInterface("client.dll", "VClient0");
+    g_pClient = (CClientBase *)util::EasyInterface(static_cast<std::string>(LIT(("client.dll"))).c_str(), static_cast<std::string>(LIT(("VClient0"))).c_str());
     printf("client: 0x%p\n", (void *)g_pClient);
-    g_pEngine = (IVEngineClient *)util::EasyInterface("engine.dll", "VEngineClient0");
+    g_pEngine = (IVEngineClient *)util::EasyInterface(static_cast<std::string>(LIT(("engine.dll"))).c_str(), static_cast<std::string>(LIT(("VEngineClient0"))).c_str());
     printf("engine: 0x%p\n", (void *)g_pEngine);
-    g_pEngineTrace = (IEngineTrace *)util::EasyInterface("engine.dll", "EngineTraceClient0");
+    g_pEngineTrace = (IEngineTrace *)util::EasyInterface(static_cast<std::string>(LIT(("engine.dll"))).c_str(), static_cast<std::string>(LIT(("EngineTraceClient0"))).c_str());
     printf("engineTrace: 0x%p\n", (void *)g_pEngineTrace);
-    g_pEntityList = (IClientEntityList *)util::EasyInterface("client.dll", "VClientEntityList0");
+    g_pEntityList = (IClientEntityList *)util::EasyInterface(static_cast<std::string>(LIT(("client.dll"))).c_str(), static_cast<std::string>(LIT(("VClientEntityList0"))).c_str());
     printf("entityList: 0x%p\n", (void *)g_pEntityList);
     DWORD *clienttable = (DWORD *)*(DWORD *)g_pClient;
     g_pClientMode = **(IClientModeShared ***)((*(DWORD **)g_pClient)[10] + 0x5);
     printf("clientmode: 0x%p\n", (void *)g_pClientMode);
     g_pGlobals = **(CGlobalVars ***)((*(DWORD **)g_pClient)[0] + 0x1B);
-    g_pGamemovement = (CGameMovement *)util::EasyInterface("client.dll", "GameMovement00");
+    g_pGamemovement = (CGameMovement *)util::EasyInterface(static_cast<std::string>(LIT(("client.dll"))).c_str(), static_cast<std::string>(LIT(("GameMovement00"))).c_str());
     printf("gamemovement: 0x%p\n", (void *)g_pGamemovement);
     if (!g_pGlobals)
         g_pGlobals = **(CGlobalVars ***)(util::findpattern(clienttable[0], 0x100, "\xA3") + 0x01); //http://www.unknowncheats.me/forum/source-engine/160691-finding-globalvars-internally-without-having-have-any-kinda-reversing-knowledge.html
     printf("global: 0x%p\n", (void *)g_pGlobals);
-    g_pPanel = (IPanel *)util::EasyInterface("vgui2.dll", "VGUI_Panel0");
+    g_pPanel = (IPanel *)util::EasyInterface(static_cast<std::string>(LIT(("vgui2.dll"))).c_str(), static_cast<std::string>(LIT(("VGUI_Panel0"))).c_str());
     printf("panel: 0x%p\n", (void *)g_pPanel);
-    g_pSurface = (ISurface *)util::EasyInterface("vguimatsurface.dll", "VGUI_Surface0");
+    g_pSurface = (ISurface *)util::EasyInterface(static_cast<std::string>(LIT(("vguimatsurface.dll"))).c_str(), static_cast<std::string>(LIT(("VGUI_Surface0"))).c_str());
     printf("surface: 0x%p\n", (void *)g_pSurface);
     
-    g_pDebugOverlay = (IVDebugOverlay *)util::EasyInterface("engine.dll", "VDebugOverlay00");
+    g_pDebugOverlay = (IVDebugOverlay *)util::EasyInterface(static_cast<std::string>(LIT(("engine.dll"))).c_str(), static_cast<std::string>(LIT(("VDebugOverlay00"))).c_str());
     printf("debugoverlay: 0x%p\n", (void *)g_pDebugOverlay);
     
-    g_pInputSystem = (IInputSystem *)util::EasyInterface("inputsystem.dll", "InputSystemVersion00");
+    g_pInputSystem = (IInputSystem *)util::EasyInterface(static_cast<std::string>(LIT(("inputsystem.dll"))).c_str(), static_cast<std::string>(LIT(("InputSystemVersion00"))).c_str());
     printf("inputSystem: 0x%p\n", (void *)g_pInputSystem);
 
     VMTClientMode.Initialise((DWORD *)g_pClientMode);
