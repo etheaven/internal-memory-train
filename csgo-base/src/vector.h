a0f3d7a0a4f44942475cb366189b40a360bd3989
BuildTools <mojweb123@gmail.com>
2017-11-03 23:01:38 +0100

    Removed mess

--- csgo-base/src/vector.h
+++ csgo-base/src/vector.h
@@ -32,14 +32,6 @@ class Vector
     bool operator!=(const float &);
     void clamp();
     void NormalizeAngles();
-    /* inline float Length();
-    vec_t Length2D() const;
-    vec_t Length2DSqr() const;
-    vec_t DistTo(Vector &vOther);
-    vec_t DotProduct(const Vector &a, const Vector &b);
-    vec_t Dot(const Vector &vOther);
-    float VectorLength(const Vector &v); */
-    //void VectorSubtract(Vector const &a, Vector const &b, Vector &c);
     float isqrt(float x);
     vec_t DotProduct(const Vector &a, const Vector &b)
     {
@@ -149,37 +141,6 @@ float Vector::operator[](int i) const
 {
     return ((float *)this)[i];
 }
-/* 
-inline float Vector::isqrt(float x)
-{
-    return (sqrtf(x));
-}
-
-inline vec_t Vector::DistTo(Vector &vOther)
-{
-    Vector delta = (*this - vOther); //VectorSubtract(*this, vOther, delta);
-    return delta.Length();
-}
-
-inline float Vector::VectorLength(const Vector &v)
-{
-    return (float)isqrt(v.x * v.x + v.y * v.y + v.z * v.z);
-}
-
-inline vec_t Vector::Length(void)
-{
-    return VectorLength(*this);
-}
-
-inline vec_t Vector::Length2D() const
-{
-    return (vec_t)::sqrtf(this->x * this->x + this->y * this->y);
-}
-
-inline vec_t Vector::Length2DSqr() const
-{
-    return (this->x * this->x + this->y * this->y);
-} */
 
 Vector::Vector()
 {
@@ -289,91 +250,6 @@ bool Vector::operator!=(const float &x)
 #define RAD2DEG(x) ((float)(x) * (float)(180.0f / (float)(PI)))
 #define RADPI 57.295779513082f
 #define rad(a) a * 0.01745329251
-/* 
-inline vec_t Vector::DotProduct(const Vector &a, const Vector &b)
-{inline vec_t DotProduct(const Vector& a, const Vector& b)
-{
-	return (a.x * b.x + a.y * b.y + a.z * b.z);
-}
-
-inline vec_t Dot(const Vector& vOther) const
-{
-	return DotProduct(*this, vOther);
-}
-
-    return (a.x * b.x + a.y * b.y + a.z * b.z);
-}
-
-inline vec_t Vector::Dot(const Vector &vOther)
-{
-    return DotProduct(*this, vOther);
-}
-
-
-void SinCos(float a, float *s, float *c)
-{
-    *s = sin(a);
-    *c = cos(a);
-}
-
-void AngleVectors(const Vector &angles, Vector *forward, Vector *right, Vector *up)
-{
-    float sr, sp, sy, cr, cp, cy;
-
-    SinCos(DEG2RAD(angles.y), &sy, &cy);
-    SinCos(DEG2RAD(angles.x), &sp, &cp);
-    SinCos(DEG2RAD(angles.z), &sr, &cr);
-
-    if (forward)
-    {
-        forward->x = cp * cy;
-        forward->y = cp * sy;
-        forward->z = -sp;
-    }
-
-    if (right)
-    {
-        right->x = (-1 * sr * sp * cy + -1 * cr * -sy);
-        right->y = (-1 * sr * sp * sy + -1 * cr * cy);
-        right->z = -1 * sr * cp;
-    }
-
-    if (up)
-    {
-        up->x = (cr * sp * cy + -sr * -sy);
-        up->y = (cr * sp * sy + -sr * cy);
-        up->z = cr * cp;
-    }
-}
-
-void Normalize(Vector &vIn, Vector &vOut)
-{
-    float flLen = vIn.Length();
-    if (flLen == 0)
-    {
-        vOut = Vector(0.f, 0.f, 1.f);
-        return;
-    }
-    flLen = 1 / flLen;
-    vOut = Vector(vIn.x * flLen, vIn.y * flLen, vIn.z * flLen);
-}
-
-void AngleVectors(const Vector &angles, Vector *forward)
-{
-
-    float sp, sy, cp, cy;
-
-    sy = sin(DEG2RAD(angles.y));
-    cy = cos(DEG2RAD(angles.y));
-
-    sp = sin(DEG2RAD(angles.x));
-    cp = cos(DEG2RAD(angles.x));
-
-    forward->x = cp * cy;
-    forward->y = cp * sy;
-    forward->z = -sp;
-} */
-
 using vec3f = Vector;
 
 class VectorAligned : public vec3f
