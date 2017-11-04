5fd93d1345f983aecb2bb1896a128ac4659fa5be
BuildTools <mojweb123@gmail.com>
2017-11-04 09:48:50 +0100

    Disabled aimbot

--- csgo-base/src/createmove.h
+++ csgo-base/src/createmove.h
@@ -243,7 +243,7 @@ bool __fastcall hkCreateMove(void *, void *, float, CUserCmd *cmd)
 	if (g_pMenu->cf_bhop.enabled)
 		bhop(cmd, local);
 	if (g_pMenu->cf_aim.enabled)
-		continue;//aimbot(cmd, local);
+		return 0;
 	if (g_pMenu->cf_rcs.enabled)
 		rcs(cmd,local);
 	if (g_pMenu->cf_trigger.enabled)
